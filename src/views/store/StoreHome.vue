8d7faf99f678aaa3f8ed3ace04fe0123de1e67b1
FSZERO <3203166692@qq.com>
2020-06-30 11:05:47 +0800

    书城界面完成

--- src/views/store/StoreHome.vue
+++ src/views/store/StoreHome.vue
@@ -3,69 +3,24 @@
     <SearchBar></SearchBar>
     <flap-card :data="random"></flap-card>
     <scroll :top='scrollTop' @onScroll="onScroll" ref="scroll">
-      <div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
-        <div>111111111111111111111111111111</div>
+      <!--图片-->
+      <div class="banner-wrapper">
+        <img :src="banner" class="banner-img">
       </div>
+      <!-- 猜你喜欢-->
+      <guess-you-like :data="guessYouLike"></guess-you-like>
+      <!--热门推荐-->
+      <recommend :data="recommend" class="recommend"></recommend>
+      <!--精选 -->
+      <featured :btn-text="$t('home.seeAll')" :data="featured"
+                :title-text="$t('home.featured')" class="featured">
+      </featured>
+      <!--下面的分类-->
+      <div :key="index" class="category-list-wrapper" v-for="(item,index) in categoryList">
+        <category-book :data="item"></category-book>
+      </div>
+      <!--所有分类-->
+      <category :data="categories" class="categories"></category>
     </scroll>
   </div>
 </template>
@@ -75,10 +30,20 @@
   import { storeHomeMixin } from '../../utils/mixin'
   import FlapCard from '../../components/home/FlapCard'
   import { home } from '../../api/store'
+  import GuessYouLike from '../../components/home/GuessYouLike'
+  import Recommend from '../../components/home/Recommend'
+  import Featured from '../../components/home/Featured'
+  import CategoryBook from '../../components/home/CategoryBook'
+  import Category from '../../components/home/Category'
 
   export default {
     mixins: [storeHomeMixin],
     components: {
+      Category,
+      CategoryBook,
+      Featured,
+      Recommend,
+      GuessYouLike,
       Scroll,
       SearchBar,
       FlapCard
@@ -98,15 +63,28 @@
     data () {
       return {
         scrollTop: 84,
-        random: null
+        random: null,
+        banner: null,
+        guessYouLike: null,
+        recommend: null,
+        featured: null,
+        categoryList: null,
+        categories: null
       }
     },
     mounted () {
       home().then(response => {
         if (response && response.status === 200) {
           const data = response.data
+          console.log(data)
           const randomIndex = Math.floor(Math.random() * data.random.length)/*向下取整,随机拿书*/
-          this.random = data.random[randomIndex]
+          this.random = data.random[randomIndex]/*书*/
+          this.banner = data.banner/*图*/
+          this.guessYouLike = data.guessYouLike/*猜你喜欢*/
+          this.recommend = data.recommend/*热门推荐*/
+          this.featured = data.featured/*精选*/
+          this.categoryList = data.categoryList/*分类*/
+          this.categories = data.categories/*所有分类*/
         }
       })
     }
@@ -118,51 +96,35 @@
   @import "../../assets/styles/global";
 
   .store-home {
-    position: relative;
     width: 100%;
     height: 100%;
-    background: #fff;
-    font-size: px2rem(16);
-    color: #666;
 
-    .book-list-wrapper {
+    .banner-wrapper {
       width: 100%;
-      overflow-x: hidden;
-      overflow-y: scroll;
-      -webkit-overflow-scrolling: touch;
+      padding: px2rem(10);
+      box-sizing: border-box;
 
-      &::-webkit-scrollbar {
-        display: none;
-      }
-
-      .banner-wrapper {
+      .banner-img {
         width: 100%;
-        padding: px2rem(10);
-        box-sizing: border-box;
-
-        .banner {
-          width: 100%;
-          height: px2rem(150);
-          background-repeat: no-repeat;
-          background-size: 100% 100%;
-        }
+        height: px2rem(100);
       }
 
-      .recommend {
-        margin-top: px2rem(20);
-      }
+    }
 
-      .featured {
-        margin-top: px2rem(20);
-      }
+    .recommend {
+      margin-top: px2rem(20);
+    }
 
-      .category-list-wrapper {
-        margin-top: px2rem(20);
-      }
+    .featured {
+      margin-top: px2rem(20);
+    }
 
-      .category {
-        margin-top: px2rem(20);
-      }
+    .category-list-wrapper {
+      margin-top: px2rem(20);
+    }
+
+    .categories {
+      margin-top: px2rem(20);
     }
   }
 </style>
